96264a2e31c5733aa79968cda48f678f25178f3f
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-12 15:02:46 +0100

    bug fixes

--- pusher.ts
+++ pusher.ts
@@ -4,7 +4,7 @@ import ClientPusher from "pusher-js";
 export const serverPusher = new Pusher({
   appId: "1505782",
   key: "c712813f8631d768fee9",
-  secret: process.env.PUSHER_SECRET,
+  secret: process.env.PUSHER_SECRET!,
   cluster: "eu",
   useTLS: true,
 });
