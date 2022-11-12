369e421b10ae90edccd51c4229218ffae96b4dac
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-12 15:00:34 +0100

    Complete server side and Loading component

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -1,23 +1,47 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import useSWR from "swr";
+import { clientPusher } from "../pusher";
 import { Message } from "../typings";
 import fetcher from "../utils/fetcMessages";
 import MessageComponent from "./MessageComponent";
 
-type Props = {};
+type Props = {
+  initialMessages: Message[];
+};
 
-function MessageList({}: Props) {
+function MessageList({ initialMessages }: Props) {
   const {
     data: messages,
     error,
     mutate,
   } = useSWR<Message[]>("/api/getMessages", fetcher);
 
+  useEffect(() => {
+    const channel = clientPusher.subscribe("messages");
+
+    channel.bind("new-message", async (data: Message) => {
+      //If you sent the message, no need to update cache
+      if (messages?.find((message) => message.id === data.id)) return;
+      if (!messages) {
+        mutate(fetcher);
+      } else {
+        mutate(fetcher, {
+          optimisticData: [data, ...messages!],
+          rollbackOnError: true,
+        });
+      }
+    });
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages, mutate, clientPusher]);
+
   return (
     <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
-      {messages?.map((message) => (
+      {(messages || initialMessages).map((message) => (
         <MessageComponent key={message.id} message={message} />
       ))}
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import { Message } from "../typings";
 import ChatInput from "./ChatInput";
 import MessageList from "./MessageList";
 
 type Props = {};
 
-function HomePage({}: Props) {
+async function HomePage({}: Props) {
+  const data = await fetch(
+    `${process.env.VERCEL_URL || "http://localhost:3000"}/api/getMessages`
+  ).then((res) => res.json());
+
+  const messages: Message[] = data.messages;
   return (
     <main className="">
       {/* Message list */}
-      <MessageList />
+      <MessageList initialMessages={messages} />
       {/* Chat out */}
       <ChatInput />
     </main>
